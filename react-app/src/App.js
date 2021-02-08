8c61b047886c3da5b07f957520106cbe17a80ccd
NRSingh007 <naorem.sana@gmail.com>
2021-02-08 19:22:19 +0530

    todo

---
+++ react-app/src/App.js
@@ -0,0 +1,112 @@
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: [],
+    };
+  }
+
+  addItem(todoValue) {
+    if (todoValue !== "") {
+      const newItem = {
+        id: Date.now(),
+        value: todoValue,
+        isDone: false,
+      };
+      const list = [...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list,
+        newItem: "",
+      });
+    }
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedList = list.filter((item) => item.id !== id);
+    this.setState({ list: updatedList });
+  }
+
+  updateInput(input) {
+    this.setState({ newItem: input });
+  }
+
+  render() {
+    return (
+      <div>
+        <br />
+        <br />
+        <div className="App">
+          <img src={logo} className="App-logo" />
+          <br />
+          <br />
+          <h1 className="App">NAOREM TODO LIST</h1>
+
+          <a className="App-link" href="https://github.com/NRSingh007">
+            Visit NAOREM github profile
+          </a>
+        </div>
+
+        <br />
+        <br />
+
+        <div className="container">
+          Add an Item to your todo list...
+          <br />
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Write a TODO"
+            required
+            value={this.state.newItem}
+            onChange={(e) => this.updateInput(e.target.value)}
+          />
+          <button
+            className="add-btn"
+            onClick={() => this.addItem(this.state.newItem)}
+            disabled={!this.state.newItem.length}
+          >
+            Add TODO
+          </button>
+          <div className="list">
+            <ul>
+              {this.state.list.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <input
+                      type="checkbox"
+                      name="idDone"
+                      checked={item.isDone}
+                      onChange={() => {}}
+                    />
+                    {item.value}
+                    <button
+                      className="btn"
+                      onClick={() => this.deleteItem(item.id)}
+                    >
+                      Delete
+                    </button>
+                  </li>
+                );
+              })}
+              <li>
+                <input type="checkbox" name="" id="" />
+                Add new items
+                <button className="btn">Delete</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
